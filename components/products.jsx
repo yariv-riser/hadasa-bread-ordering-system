553614c4fe9658888a31078ac4566625c778ef82
Yariv Fruend <yarivfrd@gmail.com>
2026-01-27 17:40:05 +0200

    Fixed product sorting

--- components/products.jsx
+++ components/products.jsx
@@ -3,9 +3,14 @@ import ProductItem from './product-item';
 import classes from './products.module.css';
 
 export default function Products({ onProductSelection, quantities, products }) {
+
+  const sortedDescending = [...products].sort((a, b) => {
+    return b.created_at.localeCompare(a.created_at);
+  });
+
   return (
     <ul className={classes['product-list']}>
-      {products.map(({ name, price }) => (
+      {sortedDescending.map(({ name, price }) => (
         <li key={name}>
           <ProductItem
             name={name}
